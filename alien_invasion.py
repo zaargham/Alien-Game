1e581327654f9f56ffd6d118e8cded76647efec8
zaargham <48851583+zaargham@users.noreply.github.com>
2020-06-04 22:47:41 +0530

    Add files via upload
    initial commit

---
+++ alien_invasion.py
@@ -0,0 +1,37 @@
+import sys
+import pygame
+from settings import Settings
+
+from ship import Ship
+
+import game_functions as gf
+
+from pygame.sprite import Group
+
+def run_game():
+     # Initialize game and create a screen object.
+     pygame.init()
+     ai_settings = Settings()
+     #screen = pygame.display.set_mode((ai_settings.screen_height, ai_settings.screen_width))
+     screen = pygame.display.set_mode((800, 500))
+     pygame.display.set_caption("Alien Invasion")
+     ship = Ship(ai_settings,screen)
+     bullets = Group()
+
+
+       # Start the main loop for the game.
+
+     while True:	 
+       gf.check_events(ai_settings,screen,ship,bullets)
+       ship.update()
+       gf.update_bullets(bullets)
+       gf.update_screen(ai_settings,screen,ship,bullets)
+       for event in pygame.event.get():
+          if event.type == pygame.QUIT:
+              sys.exit()
+        # Make the most recently drawn screen visible. z
+       screen.fill(ai_settings.bg_color)
+       ship.blitme()
+       pygame.display.flip()
+
+run_game()
---
+++ check.py
@@ -0,0 +1,8 @@
+from settings import Settings
+def check():
+
+    ob=Settings()
+    print((ob.screen_height))
+    print((ob.screen_bredth))
+    print('h')
+check
